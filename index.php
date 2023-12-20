3c3506dd5d34a67e3c4b80f01cb5ce1b6c79ff31
Danil <zell123456789@yandex.ru>
2023-12-20 21:06:10 +0500

    fixs

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 
 use core\Route;
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//dev mode
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+
 session_start();
 require_once 'vendor/autoload.php';
 

8094a4324b08813a761710fd0bb6eb356e7d1f2e
Danil <zell123456789@yandex.ru>
2023-12-22 09:49:28 +0500

    213

--- dump.sql
+++ dump.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Хост: localhost:3306
--- Время создания: Сен 01 2021 г., 20:41
--- Версия сервера: 8.0.26-0ubuntu0.20.04.2
--- Версия PHP: 8.0.10
+-- Хост: gms-db:3306
+-- Время создания: Дек 22 2023 г., 04:49
+-- Версия сервера: 5.7.44
+-- Версия PHP: 8.1.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `gms`
+-- База данных: `app_db`
 --
 
 -- --------------------------------------------------------
@@ -29,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `ga_backup` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `date_create` int NOT NULL,
+  `date_create` int(11) NOT NULL,
   `hash` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,11 +42,11 @@ CREATE TABLE `ga_backup` (
 --
 
 CREATE TABLE `ga_code_colors` (
-  `id` int NOT NULL,
-  `code` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'none',
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `activ` int DEFAULT '1'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL,
+  `code` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'none',
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `activ` int(11) DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Дамп данных таблицы `ga_code_colors`
@@ -56,7 +55,6 @@ CREATE TABLE `ga_code_colors` (
 INSERT INTO `ga_code_colors` (`id`, `code`, `name`, `activ`) VALUES
 (1, '#ff99ca', 'Красный', 1),
 (2, '#99d0ff', 'Синий', 1),
-(3, '#ccfcd4', 'Салатовый', 1),
 (4, '#fbff2e', 'Жёлтый', 1);
 
 -- --------------------------------------------------------
@@ -66,13 +64,13 @@ INSERT INTO `ga_code_colors` (`id`, `code`, `name`, `activ`) VALUES
 --
 
 CREATE TABLE `ga_comments` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `moderation` decimal(1,0) NOT NULL,
-  `id_user` int NOT NULL,
-  `id_server` int NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_server` int(11) NOT NULL,
   `text` text NOT NULL,
-  `date_create` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `date_create` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -81,25 +79,25 @@ CREATE TABLE `ga_comments` (
 --
 
 CREATE TABLE `ga_games` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `code` varchar(255) NOT NULL,
   `game` varchar(255) NOT NULL,
   `status` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `ga_games`
 --
 
 INSERT INTO `ga_games` (`id`, `code`, `game`, `status`) VALUES
-(1, 'cs', 'Counter Strike 1.6', '1'),
-(2, 'csgo', 'Counter-Strike: Global Offensive', '1'),
-(3, 'css', 'Counter Strike Sourse', '1'),
-(4, 'tf2', 'Team Fortress 2', '1'),
-(5, 'ld2', 'Left 4 Dead 2', '1'),
-(6, 'rust', 'Rust', '1'),
-(7, 'samp', 'San Andreas Multiplayer', '1'),
-(8, 'mta', 'Multi Theft Auto', '1');
+(1, 'cs', 'Counter Strike 1.6', 1),
+(2, 'csgo', 'Counter-Strike: Global Offensive', 1),
+(3, 'css', 'Counter Strike Sourse', 1),
+(4, 'tf2', 'Team Fortress 2', 1),
+(5, 'ld2', 'Left 4 Dead 2', 1),
+(6, 'rust', 'Rust', 1),
+(7, 'samp', 'San Andreas Multiplayer', 1),
+(8, 'mta', 'Multi Theft Auto', 1);
 
 -- --------------------------------------------------------
 
@@ -108,11 +106,11 @@ INSERT INTO `ga_games` (`id`, `code`, `game`, `status`) VALUES
 --
 
 CREATE TABLE `ga_logs_vote` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `ip` varchar(255) NOT NULL,
   `cookie` varchar(255) NOT NULL,
-  `date_create` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `date_create` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -121,11 +119,18 @@ CREATE TABLE `ga_logs_vote` (
 --
 
 CREATE TABLE `ga_news` (
-  `id` int NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `text` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `date_create` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date_create` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `ga_news`
+--
+
+INSERT INTO `ga_news` (`id`, `title`, `text`, `date_create`) VALUES
+(1, 'Добро пожаловать', 'Добро пожаловать', 1703131351);
 
 -- --------------------------------------------------------
 
@@ -134,11 +139,11 @@ CREATE TABLE `ga_news` (
 --
 
 CREATE TABLE `ga_pages` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `text` text NOT NULL,
-  `date_create` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `date_create` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `ga_pages`
@@ -155,14 +160,14 @@ INSERT INTO `ga_pages` (`id`, `title`, `text`, `date_create`) VALUES
 --
 
 CREATE TABLE `ga_pay_logs` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `content` text NOT NULL,
-  `id_user` int DEFAULT NULL,
-  `date_create` int NOT NULL,
-  `pay_methods` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `date_create` int(11) NOT NULL,
+  `pay_methods` varchar(255) DEFAULT NULL,
   `status` varchar(255) NOT NULL,
   `bill_id` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -171,22 +176,22 @@ CREATE TABLE `ga_pay_logs` (
 --
 
 CREATE TABLE `ga_pay_methods` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `status` decimal(10,0) NOT NULL,
   `name` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `typeCode` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `ga_pay_methods`
 --
 
 INSERT INTO `ga_pay_methods` (`id`, `status`, `name`, `content`, `typeCode`) VALUES
-(1, '0', 'Robokassa', '{\"login\":\"\",\"password1\":\"\",\"password2\":\"\"}', 'robokassa'),
-(2, '0', 'UnitPay', '{\"public_key\":\"\",\"secret_key\":\"\"}', 'unitpay'),
-(3, '0', 'Free-Kassa', '{\"fk_id\":\"\",\"fk_key1\":\"\",\"fk_key2\":\"\"}', 'freekassa'),
-(4, '0', 'Qiwi p2p', '{\"public_key\":\"\",\"secret_key\":\"\"}', 'qiwi_p2p');
+(1, 1, 'Robokassa', '{\"login\":\"\",\"password1\":\"\",\"password2\":\"\"}', 'robokassa'),
+(2, 0, 'UnitPay', '{\"public_key\":\"\",\"secret_key\":\"\"}', 'unitpay'),
+(3, 0, 'Free-Kassa', '{\"fk_id\":\"\",\"fk_key1\":\"\",\"fk_key2\":\"\"}', 'freekassa'),
+(4, 0, 'Qiwi p2p', '{\"public_key\":\"\",\"secret_key\":\"\"}', 'qiwi_p2p');
 
 -- --------------------------------------------------------
 
@@ -195,37 +200,37 @@ INSERT INTO `ga_pay_methods` (`id`, `status`, `name`, `content`, `typeCode`) VAL
 --
 
 CREATE TABLE `ga_servers` (
-  `id` int NOT NULL,
-  `status` int NOT NULL,
-  `moderation` int NOT NULL,
-  `id_user` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `moderation` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `game` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
   `port` varchar(255) NOT NULL,
   `hostname` varchar(300) DEFAULT NULL,
   `map` varchar(300) DEFAULT NULL,
-  `players` int DEFAULT NULL,
-  `max_players` int DEFAULT NULL,
-  `rating` int NOT NULL DEFAULT '0',
-  `befirst_enabled` int DEFAULT '0',
-  `top_enabled` int DEFAULT '0',
-  `vip_enabled` int DEFAULT '0',
+  `players` int(11) DEFAULT NULL,
+  `max_players` int(11) DEFAULT NULL,
+  `rating` int(11) NOT NULL DEFAULT '0',
+  `befirst_enabled` int(11) DEFAULT '0',
+  `top_enabled` int(11) DEFAULT '0',
+  `vip_enabled` int(11) DEFAULT '0',
   `color_enabled` varchar(255) DEFAULT '0',
-  `gamemenu_enabled` int DEFAULT '0',
-  `date_add` int NOT NULL,
-  `top_expired_date` int DEFAULT NULL,
-  `vip_expired_date` int DEFAULT NULL,
-  `color_expired_date` int DEFAULT NULL,
-  `gamemenu_expired_date` int DEFAULT NULL,
-  `boost` int DEFAULT '0',
-  `boost_position` int DEFAULT '0',
+  `gamemenu_enabled` int(11) DEFAULT '0',
+  `date_add` int(11) NOT NULL,
+  `top_expired_date` int(11) DEFAULT NULL,
+  `vip_expired_date` int(11) DEFAULT NULL,
+  `color_expired_date` int(11) DEFAULT NULL,
+  `gamemenu_expired_date` int(11) DEFAULT NULL,
+  `boost` int(11) DEFAULT '0',
+  `boost_position` int(11) DEFAULT '0',
   `country` varchar(64) NOT NULL,
-  `ban` int DEFAULT '0',
+  `ban` int(11) DEFAULT '0',
   `ban_couse` varchar(300) DEFAULT NULL,
-  `ban_date` int DEFAULT NULL,
-  `verification_rand` int DEFAULT NULL,
+  `ban_date` int(11) DEFAULT NULL,
+  `verification_rand` int(11) DEFAULT NULL,
   `description` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -234,13 +239,13 @@ CREATE TABLE `ga_servers` (
 --
 
 CREATE TABLE `ga_services` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `period` int NOT NULL,
-  `price` int NOT NULL,
+  `period` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
   `params` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `ga_services`
@@ -248,12 +253,12 @@ CREATE TABLE `ga_services` (
 
 INSERT INTO `ga_services` (`id`, `name`, `type`, `period`, `price`, `params`) VALUES
 (2, 'TOP на 30 дней', 'top', 30, 1, ''),
-(3, 'Boost 1 круг', 'boost', 1, 1, ''),
 (4, 'VIP на 30 дней', 'vip', 30, 1, ''),
 (5, 'Выделение цветом 30 дней', 'color', 30, 1, ''),
 (6, 'Gamemenu на 30 дней', 'gamemenu', 30, 100, ''),
 (7, '100 Голосов для сервера', 'votes', 100, 1, ''),
-(8, 'Разбан сервера с бана', 'razz', 0, 1, '');
+(8, 'Разбан сервера с бана', 'razz', 0, 1, ''),
+(9, '123', 'top', 132, 200, '');
 
 -- --------------------------------------------------------
 
@@ -262,18 +267,18 @@ INSERT INTO `ga_services` (`id`, `name`, `type`, `period`, `price`, `params`) VA
 --
 
 CREATE TABLE `ga_settings` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `status_site` decimal(10,0) NOT NULL,
   `last_update_servers` decimal(10,0) NOT NULL,
   `content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `ga_settings`
 --
 
 INSERT INTO `ga_settings` (`id`, `status_site`, `last_update_servers`, `content`) VALUES
-(1, '1', '1630483426', '{\"global_settings\":{\"site_name\":\"Gms v2.0 - \\u0432\\u0435\\u0431 \\u0434\\u0432\\u0438\\u0436\\u043e\\u043a\",\"expired_time_payment\":\"1\",\"auto_add_server\":\"1\",\"count_servers_main\":\"10\",\"count_servers_top\":\"5\",\"count_servers_vip\":\"50\",\"count_servers_boost\":\"30\",\"count_servers_color\":\"20\",\"count_servers_gamemenu\":\"5\",\"top_on\":\"1\",\"boost_on\":\"1\",\"vip_on\":\"1\",\"color_on\":\"1\",\"gamemenu_on\":\"1\",\"votes_on\":\"1\",\"cron_key\":\"123\",\"auto_backup_database\":\"0\"},\"comments\":{\"guest_allow\":\"0\",\"moderation\":\"0\"}}');
+(1, 1, 1703133063, '{\"global_settings\":{\"site_name\":\"Gms - \\u0432\\u0435\\u0431 \\u0434\\u0432\\u0438\\u0436\\u043e\\u043a\",\"expired_time_payment\":\"1\",\"auto_add_server\":\"1\",\"count_servers_main\":\"10\",\"count_servers_top\":\"5\",\"count_servers_vip\":\"50\",\"count_servers_boost\":\"30\",\"count_servers_color\":\"20\",\"count_servers_gamemenu\":\"5\",\"top_on\":\"1\",\"boost_on\":\"1\",\"vip_on\":\"1\",\"color_on\":\"1\",\"gamemenu_on\":\"1\",\"votes_on\":\"1\",\"cron_key\":\"123\",\"auto_backup_database\":\"0\"},\"comments\":{\"guest_allow\":\"0\",\"moderation\":\"0\"}}');
 
 -- --------------------------------------------------------
 
@@ -282,27 +287,27 @@ INSERT INTO `ga_settings` (`id`, `status_site`, `last_update_servers`, `content`
 --
 
 CREATE TABLE `ga_users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL DEFAULT 'user',
   `password` varchar(300) NOT NULL,
   `email` varchar(300) NOT NULL,
-  `hash` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `hash` varchar(300) DEFAULT NULL,
   `balance` decimal(11,0) NOT NULL DEFAULT '9',
-  `img` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '/public/img/avatar.png',
-  `date_reg` int NOT NULL,
-  `params` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `api_login` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `reset_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `img` varchar(300) DEFAULT '/public/img/avatar.png',
+  `date_reg` int(11) NOT NULL,
+  `params` text,
+  `api_login` varchar(255) DEFAULT NULL,
+  `reset_code` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `ga_users`
 --
 
 INSERT INTO `ga_users` (`id`, `lastname`, `firstname`, `role`, `password`, `email`, `hash`, `balance`, `img`, `date_reg`, `params`, `api_login`, `reset_code`) VALUES
-(4, 'System', 'Admin', 'admin', '$2y$10$ZtVHAPch2ebEQQwleSLId.W4Hr1F/qyaLqLihN63rDFTDVVeXHbpO', 'admin@gamems.ru', 'a87ff679a2f3e71d9181a67b7542122c', '0', '/public/img/avatar.png	', 1629893904, '{\"key_api\":\"<br \\/><b>Notice<\\/b>:  Trying to access array offset on value of type null in <b>\\/home\\/danil\\/sites\\/gms.loc\\/template\\/new-style\\/control\\/users\\/edit.tpl<\\/b> on line <b>78<\\/b><br \\/>\",\"discount_api\":\"\"}', '', NULL);
+(4, 'System', 'Admin', 'admin', '$2y$10$ZtVHAPch2ebEQQwleSLId.W4Hr1F/qyaLqLihN63rDFTDVVeXHbpO', 'admin@gamems.ru', 'a87ff679a2f3e71d9181a67b7542122c', 0, '/public/img/avatar.png	', 1629893904, '{\"key_api\":\"<br \\/><b>Notice<\\/b>:  Trying to access array offset on value of type null in <b>\\/home\\/danil\\/sites\\/gms.loc\\/template\\/new-style\\/control\\/users\\/edit.tpl<\\/b> on line <b>78<\\/b><br \\/>\",\"discount_api\":\"\"}', '', NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -394,79 +399,79 @@ ALTER TABLE `ga_users`
 -- AUTO_INCREMENT для таблицы `ga_backup`
 --
 ALTER TABLE `ga_backup`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_code_colors`
 --
 ALTER TABLE `ga_code_colors`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_comments`
 --
 ALTER TABLE `ga_comments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_games`
 --
 ALTER TABLE `ga_games`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_logs_vote`
 --
 ALTER TABLE `ga_logs_vote`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_news`
 --
 ALTER TABLE `ga_news`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_pages`
 --
 ALTER TABLE `ga_pages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_pay_logs`
 --
 ALTER TABLE `ga_pay_logs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_pay_methods`
 --
 ALTER TABLE `ga_pay_methods`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_servers`
 --
 ALTER TABLE `ga_servers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_services`
 --
 ALTER TABLE `ga_services`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_settings`
 --
 ALTER TABLE `ga_settings`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `ga_users`
 --
 ALTER TABLE `ga_users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

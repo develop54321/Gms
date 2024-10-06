afd91e57833e9ed9efd7f6ae93d3371e514cd388
Danil <zell123456789@yandex.ru>
2024-10-06 15:10:22 +0500

    last changes

--- index.php
+++ index.php
@@ -10,12 +10,11 @@ use core\Route;
 
 
 //dev mode
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-ini_set('display_errors', 'On');
-
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//
+//ini_set('display_errors', 'On');
 
 session_start();
 require_once 'vendor/autoload.php';

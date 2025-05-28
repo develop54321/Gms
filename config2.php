fdc0e6da4db9bbac154f4a49f8a538cdef478c13
Danil <zell123456789@yandex.ru>
2025-05-28 20:02:04 +0500

    файл лицензии

---
+++ config2.php
@@ -0,0 +1,9 @@
+<?php
+const BASE_URL = "http://localhost";
+const DB_HOST = "gms-db";
+const DB_USER = "db_user";
+const DB_PASSWORD = "db_user_pass";
+const DB_NAME = "app_db";
+const TMPL_DIR = "template/default2";
+const VERSION =  "3.1.3";
+

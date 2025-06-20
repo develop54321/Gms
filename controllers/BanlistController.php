80a00bc54981db3e38ac757f5a16dafd5a0a77db
Danil <zell123456789@yandex.ru>
2025-06-20 19:28:47 +0500

    add pagination

--- controllers/BanlistController.php
+++ controllers/BanlistController.php
@@ -2,21 +2,37 @@
 
 namespace controllers;
 
+use components\Pagination;
 use core\BaseController;
 
 class BanlistController extends BaseController
 {
+    private const PER_PAGE = 25;
     public function index()
     {
-        $title = "Банлист";
+        $title = "Бан лист";
 
-        $getBannisterServers = $this->db->prepare('SELECT * FROM ga_servers WHERE ban != :ban');
-        $getBannisterServers->execute(array(':ban' => '0'));
-        $getBannisterServers = $getBannisterServers->fetchAll();
+        $sort_where = array(':ban' => 0);
+
+        $queryCount = $this->db->prepare('SELECT * FROM ga_servers WHERE ban != :ban');
+        $queryCount->execute($sort_where);
+        $count = $queryCount->rowCount();
 
 
-        $content = $this->view->renderPartial("banlist", ['BannisterServers' => $getBannisterServers]);
 
+        $pagination = new Pagination();
+        $per_page = self::PER_PAGE;
+        $result = $pagination->create(array('per_page' => $per_page, 'count' => $count));
+
+
+
+        $getServers = $this->db->prepare('SELECT * FROM ga_servers WHERE ban != :ban LIMIT ' . $result['start'] . ', ' . $per_page . '');
+        $getServers->execute($sort_where);
+        $getServers = $getServers->fetchAll();
+
+        $pagination_html = $result['ViewPagination'];
+
+        $content = $this->view->renderPartial("banlist", ['servers' => $getServers, 'pagination_html' => $pagination_html]);
 
         $this->view->render("main", ['content' => $content, 'title' => $title]);
     }
